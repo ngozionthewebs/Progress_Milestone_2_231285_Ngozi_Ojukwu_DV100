ba66c85a98523f272d6e50d81b9e4ac68704b99f
ngozionthewebs <231285@virtualwindow.co.za>
2023-06-09 23:05:39 +0200

    js for build a sub

--- js/index.js
+++ js/index.js
@@ -1,4 +1,72 @@
-let subBooking = [];
+
+let subOrder = [] ;
 
 
+makeSub = () => {
+
+    let subPrice = 0
+
+    let subName = document.getElementById("subName").value;
+
+    //Get Radio Options
+    let selectedBread = document.getElementsByName("breadRadio");
+    let breadValue;
+
+    for(let i = 0; i < selectedBread.length; i++){
+        if (selectedBread[i].checked){
+            breadValue = selectedBread[i].value
+            subPrice = subPrice + +selectedBread[i].dataset.cost
+        }
+    }
+
+    
+
+    let selectedToppings = document.getElementsByName("toppings")
+    let toppingArray = [] ;
+    
+    for(let i = 0; i < selectedToppings.length; i++){
+
+        if(selectedToppings[i].checked){
+            toppingArray.push(selectedToppings[i].value) 
+            subPrice = subPrice + +selectedToppings[i].dataset.cost
+        }
+
+
+
+    }
+
+    let selectedSauce = document.getElementsByName("sauceRadio")
+    let sauceValue;
+
+    for(let i = 0; i < selectedSauce.length; i++){
+        if (selectedSauce[i].checked){
+            sauceValue = selectedSauce [i].value
+            subPrice = subPrice + +selectedSauce[i].dataset.cost
+        }
+
+        
+    }
+
+    subOrder.push({
+        subName: subName,
+        subBread: breadValue,
+        subToppings: toppingArray,
+        subSauce: sauceValue,
+        fullCost: subPrice
+
+        
+    })
+
+    
+    console.log(subOrder)
+
+    document.getElementById("subForm").reset();
+
+
+    
+    
+
+
+
+}
 
