6418d0e449212501961e020ec892353ae5126a83
ngozionthewebs <231285@virtualwindow.co.za>
2023-06-09 23:41:26 +0200

    live total js

--- js/index.js
+++ js/index.js
@@ -60,13 +60,47 @@ makeSub = () => {
     
     console.log(subOrder)
 
+    document.getElementById("liveTotal").innerHTML = "0.00"
+
     document.getElementById("subForm").reset();
 
 
-    
+}
+
+liveTotal = () => {
+
+    livePrice = 0;
+
+    let selectedBread = document.getElementsByName("breadRadio");
+    for(let i = 0; i < selectedBread.length; i++){
+        if (selectedBread[i].checked){
+            livePrice = livePrice+ +selectedBread[i].dataset.cost
+        }
+    }
+
     
 
+    let selectedToppings = document.getElementsByName("toppings")
+    for(let i = 0; i < selectedToppings.length; i++){
+        if(selectedToppings[i].checked){
+            livePrice = livePrice + +selectedToppings[i].dataset.cost
+        }
 
+    }
+
+    let selectedSauce = document.getElementsByName("sauceRadio")
+    for(let i = 0; i < selectedSauce.length; i++){
+        if (selectedSauce[i].checked){
+            livePrice = livePrice + +selectedSauce[i].dataset.cost
+        }
+        
+    }
+
+    
+
+    document.getElementById("liveTotal").innerHTML = "R" + livePrice + ".00"
+
+    
 
 }
 
